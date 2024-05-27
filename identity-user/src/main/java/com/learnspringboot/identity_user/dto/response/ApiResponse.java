0d78a3c9fd3d30db4f1fdd096edb2a3c3518a262
lekien-2803 <lekien.2803.cg@gmail.com>
2024-05-27 23:33:40 +0700

    update 5

--- identity-user/src/main/java/com/learnspringboot/identity_user/dto/response/ApiResponse.java
+++ identity-user/src/main/java/com/learnspringboot/identity_user/dto/response/ApiResponse.java
@@ -2,12 +2,15 @@ package com.learnspringboot.identity_user.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ApiResponse <T>{
-    private int code = 1000;
-    private String message;
-    private T result;
+    int code = 1000;
+    String message;
+    T result;
 }
--- identity-user/src/main/java/com/learnspringboot/identity_user/exception/AppException.java
+++ identity-user/src/main/java/com/learnspringboot/identity_user/exception/AppException.java
@@ -1,14 +1,17 @@
 package com.learnspringboot.identity_user.exception;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @AllArgsConstructor
 @Getter
 @Setter
 @NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class AppException extends RuntimeException{
-    private ErrorCode errorCode;
+    ErrorCode errorCode;
 }
